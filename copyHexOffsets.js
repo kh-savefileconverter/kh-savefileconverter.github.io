254637982d8c7be701169264d26cf589f058dc81
GillianMC <70141395+SandeMC@users.noreply.github.com>
2024-06-14 11:05:15 +0300

    remove the stupid

--- copyHexOffsets.js
+++ copyHexOffsets.js
@@ -1,9 +1,42 @@
+function areBaseNamesSame(name1, name2) {
+    const baseName1 = name1.split('.')[0].split('_')[0];
+    const baseName2 = name2.split('.')[0].split('_')[0];
+    return baseName1 === baseName2;
+}
+
 function copyHexOffsets() {
     const fileInput1 = document.getElementById('file1').files[0];
     const fileInput2 = document.getElementById('file2').files[0];
 
     if (!fileInput1 || !fileInput2) {
         alert("Please select two files.");
+        return;
+    }
+	else if (fileInput1.name.startsWith('KHIII')){
+		alert("Kingdom Hearts III savefiles are not supported.");
+		return;
+	}
+	else if (fileInput1.name.startsWith('KH0.2')){
+		alert("Kingdom Hearts 0.2 savefiles are not supported.");
+		return;
+	}
+	else if (fileInput1.name.startsWith('KH3DHD')){
+		alert("Kingdom Hearts DDD savefiles are not supported.");
+		return;
+	}
+	else if ((fileInput1.name.startsWith('KH') && !fileInput1.name.endsWith('.png')) && (!fileInput2.name.startsWith('KH') && !fileInput2.name.endsWith('.png'))){
+		alert("The files selected are not Kingdom Hearts savefiles.");
+	}
+	else if (!fileInput1.name.startsWith('KH') && !fileInput1.name.endsWith('.png')){
+		alert("File 1 (Convert from slot) is not a Kingdom Hearts savefile.");
+		return;
+	}
+	else if (!fileInput2.name.startsWith('KH') && !fileInput2.name.endsWith('.png')){
+		alert("File 2 (Convert to slot) is not a Kingdom Hearts savefile.");
+		return;
+	}
+    else if (!areBaseNamesSame(fileInput1.name, fileInput2.name)) {
+        alert("Savefiles are not of the same game.");
         return;
     }
 
@@ -25,11 +58,16 @@ function copyHexOffsets() {
 
             // Create new file
             const blob1 = new Blob([uint8Array1], { type: 'application/octet-stream' });
+            const downloadUrl = URL.createObjectURL(blob1);
 
-            const downloadLink1 = document.getElementById('download1');
-            downloadLink1.href = URL.createObjectURL(blob1);
-            downloadLink1.download = fileInput2.name;
-            downloadLink1.style.display = 'block';
+            // Trigger download
+            const a = document.createElement('a');
+            a.href = downloadUrl;
+            a.download = fileInput2.name;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            URL.revokeObjectURL(downloadUrl);
         };
 
         reader2.readAsArrayBuffer(fileInput2);
